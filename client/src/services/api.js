14ecdf423f26c92d000beb4bc49f7a54ec3a4a0e
Derrick Widner <derrick.widner@outlook.com>
2026-04-23 22:48:50 -0500

    added backend but no DB just yet (render)

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://stevies-buffet.onrender.com";
 
 export const getMenu = async () => {
     const res = await fetch(`${API_URL}/menu`);
